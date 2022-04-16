2b12f7b3b6438f0277ebeee6fe66140cbc30f2c3
Michael <work.michael.ma@gmail.com>
2022-04-16 23:43:52 +0800

    use firebase-adminsdk.json

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./price-a4709-firebase-adminsdk-68hof-07379f9757.json");
+var serviceAccount = require("./firebase-adminsdk.json");
 
 export const initFirebase = () => {
   if (!admin.apps.length) {
